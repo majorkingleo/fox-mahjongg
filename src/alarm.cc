33382b71341b4f02e7e62838ae17be79193db73a
Martin Oberzalek <kingleo@gmx.at>
2022-03-19 17:56:19 +0100

    Reformated Alarm class

--- src/alarm.cc
+++ src/alarm.cc
@@ -1,57 +1,55 @@
 #include "alarm.hh"
 
-void
-AlarmHooks::alarm()
+void AlarmHooks::alarm()
 {
 }
 
-void
-AlarmHooks::alarm(Alarm *)
+void AlarmHooks::alarm(Alarm*)
 {
-  alarm();
+	alarm();
 }
-
 
 Alarm *Alarm::alarms = 0;
 
-
-void
-Alarm::schedule(const Moment &m)
+void Alarm::schedule(const Moment &m)
 {
-  if (_scheduled) unschedule();
-  _moment = m;
-  
-  Alarm *prev = 0, *a = alarms;
-  while (a && m >= a->_moment) {
-    prev = a;
-    a = a->_next;
-  }
-  
-  if (prev) prev->_next = this;
-  else alarms = this;
-  _next = a;
-  _scheduled = true;
-  _dead = false;
+	if (_scheduled) {
+		unschedule();
+	}
+
+	_moment = m;
+
+	Alarm *prev = 0, *a = alarms;
+	while (a && m >= a->_moment) {
+		prev = a;
+		a = a->_next;
+	}
+
+	if (prev) {
+		prev->_next = this;
+	} else {
+		alarms = this;
+	}
+
+	_next = a;
+	_scheduled = true;
+	_dead = false;
 }
 
-
-void
-Alarm::unschedule()
+void Alarm::unschedule()
 {
-  if (alarms == this)
-    alarms = _next;
-  else {
-    Alarm *prev;
-    for (prev = alarms; prev->_next != this; prev = prev->_next)
-      ;
-    prev->_next = _next;
-  }
-  _scheduled = false;
+	if (alarms == this) {
+		alarms = _next;
+	} else {
+		Alarm *prev;
+		for (prev = alarms; prev->_next != this; prev = prev->_next)
+			;
+		prev->_next = _next;
+	}
+	_scheduled = false;
 }
 
-
-void
-Alarm::x_wait()
+void Alarm::x_wait()
 {
 	Moment time = Moment::now();
 
@@ -59,7 +57,8 @@ Alarm::x_wait()
 		Alarm *a = alarms;
 		alarms = a->_next;
 		a->_scheduled = false;
-		if (!a->_dead)
+		if (!a->_dead) {
 			a->_hook->alarm(a);
+		}
 	}
 }
